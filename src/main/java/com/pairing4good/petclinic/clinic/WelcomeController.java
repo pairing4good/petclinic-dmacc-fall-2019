3c6d4d2482372bee70a977c7527ef255288e9bfb
pairing4good <pairing4good@gmail.com>
2019-08-10 14:34:27 -0500

    Added the clinic name for customers

--- src/main/java/com/pairing4good/petclinic/clinic/WelcomeController.java
+++ src/main/java/com/pairing4good/petclinic/clinic/WelcomeController.java
@@ -1,13 +1,17 @@
 package com.pairing4good.petclinic.clinic;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "welcome";
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView("welcome");
+        modelAndView.addObject("petClinicName", "Bob's Pet Clinic");
+        return modelAndView;
     }
 }
